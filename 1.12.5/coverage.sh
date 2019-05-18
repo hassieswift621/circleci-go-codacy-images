2ff04b593bd7a3ce63676b114d9010f1ea54f59e
hassieswift621 <hassie.dev@gmail.com>
2019-05-18 03:25:44 +0100

    Fixes

--- 1.12.5/coverage.sh
+++ 1.12.5/coverage.sh
@@ -1,3 +1,2 @@
 go test -coverprofile=coverage.out
-ls #debug
-godacov -t $CODACY_PROJECT_TOKEN -r $CIRCLE_WORKING_DIRECTORY/coverage.out -c $CIRCLE_SHA1
+godacov -t $CODACY_PROJECT_TOKEN -r ./coverage.out -c $CIRCLE_SHA1
